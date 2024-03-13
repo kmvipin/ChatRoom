fac26eb04ece87525ba5e2fc1143ef47546656e0
Vipin <vk783838@gmail.com>
2024-03-13 14:37:55 +0530

    remove comment code

--- src/LoginForm.js
+++ src/LoginForm.js
@@ -49,62 +49,6 @@ const LoginForm = ({
 
   return (
     <div className="flex justify-center items-center h-full">
-      {/* <div className="flex justify-center items-center h-full">
-        <div className="bg-white p-8 shadow-md w-96 mx-10">
-          <form
-            id="loginForm"
-            name="loginForm"
-            onSubmit={(event) => {
-              if (!loginFormInvalid) {
-                onLogin();
-              }
-              event.preventDefault();
-            }}
-          >
-            <div className="mb-6">
-              <h1 className="my-2">Type Your Name</h1>
-              <input
-                autoFocus
-                id="name"
-                type="text"
-                placeholder="Name"
-                autoComplete="off"
-                className="border rounded-md p-2 w-full outline-none"
-                onChange={(event) => onChange(event.target.value)}
-              />
-              <h1 className="my-2">Enter Room Name</h1>
-              <input
-                autoFocus
-                id="room"
-                type="text"
-                placeholder="Room Name"
-                autoComplete="off"
-                className="border rounded-md p-2 w-full outline-none"
-                onChange={(event) => onChangeRoomName(event.target.value)}
-              />
-              <h1 className="my-2">Enter Room Code</h1>
-              <input
-                autoFocus
-                id="code"
-                type="text"
-                placeholder="Room Code"
-                autoComplete="off"
-                className="border rounded-md p-2 w-full outline-none"
-                onChange={(event) => onChangeCode(event.target.value)}
-              />
-            </div>
-            <div className="flex justify-center text-center">
-              <button
-                disabled={loginFormInvalid}
-                type="submit"
-                className="bg-blue-500 text-white py-2 px-4 rounded-md"
-              >
-                Login
-              </button>
-            </div>
-          </form>
-        </div>
-      </div> */}
       <div
         key="1"
         className="grid max-w-sm gap-4 px-4 mx-auto lg:max-w-none lg:grid-cols-2 xl:gap-8"
